1264eba9f76f3ebfd182eca81bd4ec30e4e34d6a
marcvanopstalndla <83448206+marcvanopstalndla@users.noreply.github.com>
2021-11-10 21:44:14 +0100

    Update forceNN.js

--- src/forceNN.js
+++ src/forceNN.js
@@ -2,16 +2,16 @@ videojs.registerPlugin('forceNNcaption', function() {
     var myPlayer = this;
   
     //myPlayer.on("loadedmetadata", function () {
-    myPlayer.on("playing", function () {
+    myPlayer.on("loadeddata", function () {
 
         // Get all text tracks for the current player.
         var tracks = myPlayer.textTracks();
 
         for (var i = 0; i < tracks.length; i++) {
             var track = tracks[i];
-            videojs.log('track.lang=');
-            // Find the captions track and mark it as "showing".
-            if (track.kind === 'captions' && track.language.substr(0, 2).toLowerCase() === 'nn') {
+            // Find captions track and mark it as "showing".
+            // if (track.kind === 'captions' && track.language.substr(0, 2).toLowerCase() === 'nn') {
+            if (track.language.substr(0, 2).toLowerCase() === 'nn') {
                 track.mode = 'showing';
             } else {
                 track.mode = 'disabled';
