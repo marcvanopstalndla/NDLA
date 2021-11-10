874d9f67dab77d20d94c4c40383168bb7be74665
marcvanopstalndla <83448206+marcvanopstalndla@users.noreply.github.com>
2021-11-10 15:45:51 +0100

    Update forceNN.js

--- src/forceNN.js
+++ src/forceNN.js
@@ -4,7 +4,7 @@ var tracks = player.textTracks();
 for (var i = 0; i < tracks.length; i++) {
   var track = tracks[i];
 
-  // Find the English captions track and mark it as "showing".
+  // Find the captions track and mark it as "showing".
   if (track.kind === 'captions' && track.language === 'nn') {
     track.mode = 'showing';
   }
